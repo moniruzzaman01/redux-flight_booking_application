ab1e7ea0929e6a07aae9e5357cb41a61cb37f0ed
moniruzzaman01 <moniruzzamanshakib04@gmail.com>
2023-12-30 01:03:19 +0600

    application setup complete with required components

---
+++ src/App.jsx
@@ -0,0 +1,28 @@
+import "./App.css";
+import Form from "./components/Form";
+import Header from "./components/Header";
+import Table from "./components/Table";
+
+function App() {
+  return (
+    <>
+      <Header />
+
+      <section>
+        {/* <!-- Input Data --> */}
+        <div className="mt-[160px] mx-4 md:mt-[160px] relative">
+          <div className="bg-white rounded-md max-w-6xl w-full mx-auto">
+            <Form />
+          </div>
+        </div>
+
+        {/* <!-- Preview Data --> */}
+        <div className="table-container">
+          <Table />
+        </div>
+      </section>
+    </>
+  );
+}
+
+export default App;
